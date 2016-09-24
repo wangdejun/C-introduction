e4e20f82a77943dc3af03ea0d99b382bfc4e3c8d
wangdejun <1074201799@qq.com>
2016-09-24 23:06:01 +0800

    Initial Commit

---
+++ C++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++
+//
+//  Created by 王德军 on 16/9/24.
+//  Copyright (c) 2016年 王德军. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
