4e91a7d5f8174158c62e3878c8698590c88cf0f7
wangdejun <1074201799@qq.com>
2017-06-25 19:48:50 +0800

    function

--- C/TheCProgrammingLanguage.c
+++ C/TheCProgrammingLanguage.c
@@ -188,3 +188,27 @@
 // 	}
 // }
 
+// #include <stdio.h>
+
+// int power(int m, int n);
+
+// int main()
+// {
+// 	int i;
+// 	printf("-------------------------------\n");
+// 	for(i=0;i<10;++i)
+// 	{
+// 		printf("||%1d | %10d | %10d ||\n", i, power(2,i), power(3,i));
+// 		printf("-------------------------------\n");
+// 	}
+// }
+
+// int power(int base, int n)
+// {
+// 	int i, p;
+// 	p = 1;
+// 	for(i = 1;i<=n;++i){
+// 		p = p * base;
+// 	}
+// 	return p;
+// }
