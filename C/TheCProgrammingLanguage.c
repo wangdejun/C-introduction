45f298b4d47fda54fd0a97742f79b89961482bb8
wangdejun <1074201799@qq.com>
2017-06-28 21:09:34 +0800

    something

--- C/TheCProgrammingLanguage.c
+++ C/TheCProgrammingLanguage.c
@@ -280,6 +280,54 @@
 // 	pa = &z[0];
 // 	printf("%d",*pa);
 // 	pa++;
-// 	printf("%d",*pa);	
-	
+// 	printf("%d",*pa);
+// }
+
+// #include <stdio.h>
+// #define MAXLINE 1000
+
+// int getlinee(char line[], int maxline);
+// void copy(char to[], char from[]);
+
+// int main(){
+//     int len;
+//     int max;
+//     char line[MAXLINE];
+//     char longest[MAXLINE];
+
+//     max = 0;
+//     while((len = getlinee(line, MAXLINE))>0)
+//     {
+//         if(len > max){
+//             max = len;
+//             copy(longest, line);
+//         }
+//         if(max > 0){
+//             printf("%s", longest);
+//         }
+//         return 0;
+//     }
+// }
+
+// int getlinee(char s[], int lim)
+// {
+//     int c, i;
+//     for (i=0;i<lim-1 && (c=getchar())!=EOF && c!='\n';++i)
+//     {
+//         s[i]= c;
+//     }
+//     if(c=='\n'){
+//         s[i]=c;
+//         ++i;
+//     }
+//     return i;
+// }
+
+// void copy(char to[], char from[])
+// {
+//     int i;
+//     while((to[i]=from[i])!='\0')
+//     {
+//         ++i;
+//     }
 // }
