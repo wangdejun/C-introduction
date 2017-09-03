de2e3869b978c253c6889fbffc8ef859c03526f7
wangdejun <1074201799@qq.com>
2017-09-03 15:56:59 +0800

    more variables and algrithms

--- C/LCTHW/ex7.c
+++ C/LCTHW/ex7.c
@@ -10,5 +10,11 @@ int main(int argc, char *argv[])
     long universe_of_defects = 1L * 1024L * 1024L * 1024L;
     printf("The entire universe has %ld bugs\n", universe_of_defects);
 
-    
+    double expected_bugs = bugs * bug_rate;
+    printf("You are expected to have %f bugs.\n", expected_bugs);
+
+    double part_of_universe = expected_bugs/universe_of_defects;
+    printf("That is only a %+10e portion of the universe. \n", part_of_universe);
+
+    return 0;
 }
