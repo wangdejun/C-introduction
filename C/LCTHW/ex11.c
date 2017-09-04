26d4fc627c844fdf54e80e236f48c4f38583cedd
wangdejun <1074201799@qq.com>
2017-09-04 12:20:33 +0800

    add states

--- C/LCTHW/ex11.c
+++ C/LCTHW/ex11.c
@@ -9,6 +9,17 @@ int main(int argc, char *argv[])
         printf("arg %d: %s %s %s\n", i, argv[i], *argv, argv[0]);
         i++;
     }
+    //let's make our own array of strings
 
+    char *states[] = {
+        "California", "Oregon", "Washington", "Texas"
+    };
+
+    int num_states = 4;
+    i=0; // watch for this
+    while(i<num_states){
+        printf("states %d: %s\n",i, states[i]);
+        i++;
+    }
     return 0;
 }
