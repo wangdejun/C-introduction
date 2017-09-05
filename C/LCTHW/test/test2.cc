6a18572894a3cc007ce4c92278a68d3092130d27
wangdejun <1074201799@qq.com>
2017-09-05 11:45:43 +0800

    hex dec oct transoform stream

---
+++ C/LCTHW/test/test2.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    //以ox开头的是16进制数字
+    int a = 0x7FFFFF85;
+    cout << dec << a <<endl;
+    cout << oct << a <<endl;
+
+    int b = 077;
+    cout << dec << b <<endl;
+    cout << hex << b <<endl;
+    return 0;
+}
