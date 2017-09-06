ea9f66514de5243be9d923cb9b1ec86edce37176
wangdejun <1074201799@qq.com>
2017-09-06 22:16:23 +0800

    fix

--- C/LCTHW/ex5.c
+++ C/LCTHW/ex5.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 /*This is a comment. */
 int main(int argc, char *argv[])
 {
     int distance = 100;
-
     printf("You are %d miles away.\n", distance);
 
     return 0;
--- C/LCTHW/ex6.c
+++ C/LCTHW/ex6.c
@@ -17,5 +17,6 @@ int main(int argc, char *argv[])
     printf("I have a first name %s.\n", first_name);
     printf("I have a last name %s.\n", last_name);
     printf("My whole name is %s %c. %s.\n", first_name, initial, last_name);
+    
     return 0;
 }
