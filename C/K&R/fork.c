0bbbe076a9de3562b4ab387503d8335a749cb483
wangdejun <1074201799@qq.com>
2017-11-17 21:12:09 +0800

    fix

--- C/K&R/fork.c
+++ C/K&R/fork.c
@@ -11,9 +11,11 @@ int main()
         printf("fork failed");
     }else if(pid == 0)/*对子进程来说，fork()函数返回的是值是0*/
     {
+        /*子进程空间*/
         printf("exec the child process\n");
     }else
     {
+        /*父进程空间*/
         printf("the child process pid is:%d\n", pid);
         wait(NULL);/*parent process wait for the child process to execute finish*/
         printf("child complete\n");
