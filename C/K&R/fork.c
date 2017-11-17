92e9d17c41ef4b55ff7b0449887c535ac4fe592f
wangdejun <1074201799@qq.com>
2017-11-17 21:10:45 +0800

    add a fork test

---
+++ C/K&R/fork.c
@@ -0,0 +1,22 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    pid_t pid;
+    pid = fork();/*创建一个child process*/
+    
+    if(pid<0){/*error*/
+        printf("fork failed");
+    }else if(pid == 0)/*对子进程来说，fork()函数返回的是值是0*/
+    {
+        printf("exec the child process\n");
+    }else
+    {
+        printf("the child process pid is:%d\n", pid);
+        wait(NULL);/*parent process wait for the child process to execute finish*/
+        printf("child complete\n");
+    }
+    return 0;
+}
