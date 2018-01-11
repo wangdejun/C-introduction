b604eb03cc80c98cdc050ab316dd21d2e4d08cc5
wangdejun <1074201799@qq.com>
2018-01-11 10:15:22 +0800

    change to normal sorting

--- C++/CourseraPekingU/sort.cpp
+++ C++/CourseraPekingU/sort.cpp
@@ -5,7 +5,7 @@ int MyCompare(const void * elem1, const void * elem2){
     unsigned int *p1, *p2;
     p1 = (unsigned int *) elem1;
     p2 = (unsigned int *) elem2;
-    return (*p1 % 10) - (*p2 % 10);
+    return *p1 - *p2;
 }
 
 #define NUM 5
