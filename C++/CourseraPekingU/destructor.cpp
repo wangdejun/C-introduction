6373552823af9a44978b69d140b2ef162bd35063
wangdejun <1074201799@qq.com>
2018-01-14 00:39:19 +0800

    destructor

---
+++ C++/CourseraPekingU/destructor.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+class Ctest{
+    public:
+        //destructor will be called if the program was ended;
+        ~Ctest(){
+            cout<<"destrcutor called"<<endl;
+        }
+};
+
+
+int main(){
+    //we new 2instances ,when the main program was ended;
+    //the destructor will be call 2 times;
+    Ctest array[2];
+    cout<<"End Main"<<endl;
+    return 0;
+}
