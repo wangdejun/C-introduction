40eb5460d14744580304c40e312eebaf44612ba6
wangdejun <1074201799@qq.com>
2018-01-15 13:18:48 +0800

    return a const char *

--- C++/stringclass.cpp
+++ C++/stringclass.cpp
@@ -7,7 +7,7 @@ class String{
         String():str(NULL){}
         const char * c_str(){
             return str;
-        }
+        }//返回值为const类型，不能通过指针修改指针所指向的内容
         char * operator = (const char * s);
         ~String();
 };
