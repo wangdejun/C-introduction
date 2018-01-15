69792f69c9ac56a3e7aebb790a3260cabca03276
wangdejun <1074201799@qq.com>
2018-01-15 13:41:15 +0800

    operator overload

--- C++/stringclass.cpp
+++ C++/stringclass.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
-
+#include <iostream>
+using namespace std;
+//String class
 class String{
     private:
         char *str;
@@ -11,3 +13,47 @@ class String{
         char * operator = (const char * s);
         ~String();
 };
+
+//override
+/*
+char * String::operator=(const char * s){
+    if(str)
+        delete [] str;
+    if(s){//s is NULL ,anc canbe execute the copy behavior
+        str = new char[strlen(s)+1];//new a block of memory
+        strcpy(str, s);
+    }else{
+        str = NULL;
+    }
+    return str;
+}
+*/
+//deep copy String Class meber function
+String & String::operator= (const String & s){
+    if(str==s.str)
+        return * this;
+    if(str)
+        delete [] str;
+    if(s.str){
+        str = new char[strlen(s.str)+1];
+        strcpy(str, s.str);
+    }else{
+        str = NULL;
+    }
+    return * this;//返回指针
+}
+
+String::~String(){
+    //不为空的时候，delete the pointer;
+    if(str)
+        delete [] str;
+};
+
+int main(){
+    String s;
+    s = "Good Luck";
+    cout<<s.c_str()<<endl;
+    s="Shenzhou8";
+    cout<<s.c_str()<<endl;
+    return 0;
+}
